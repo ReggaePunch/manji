6b51cbcef2e57f84a27b9555cb991392e7700f26
ReggaePunch <wakiryo@gmail.com>
2017-06-11 08:57:06 +0000

    portal

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :portals
   resources :articles
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
