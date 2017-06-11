9c9d68f2df81d65272a28a3bb52b1a3510a69b2b
ReggaePunch <wakiryo@gmail.com>
2017-06-11 12:49:13 +0000

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'portals#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -56,4 +56,7 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  
+
+  
 end
